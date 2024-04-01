0dd24c6d9e3a0645ada338ee4199b639988ec737
fadayo2 <afadunmiye@gmail.com>
2024-04-01 06:46:01 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { motion } from 'framer-motion';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <nav className='navbar'>
+        <div className='navlinks'>
+          <motion.div initial={{ opacity:1 }} whileHover={{ scale: .89 , border: " 2px solid black " , borderRight : 'none' , borderLeft : 'none' }} transition={{ ease: 'linear' , type: 'spring' , stiffness: 1000 , duration: .5 }}>Home</motion.div>
+          <motion.div initial={{ opacity:1 }} whileHover={{ scale: .89 , border: " 2px solid black " , borderRight : 'none' , borderLeft : 'none' }} transition={{ ease: 'linear' , type: 'spring' , stiffness: 1000 , duration: .5 }}>Projects</motion.div>
+          <motion.div initial={{ opacity:1 }} whileHover={{ scale: .89 , border: " 2px solid black " , borderRight : 'none' , borderLeft : 'none' }} transition={{ ease: 'linear' , type: 'spring' , stiffness: 1000 , duration: .5 }}>About</motion.div>
+          <motion.div initial={{ opacity:1 }} whileHover={{ scale: .89 , border: " 2px solid black " , borderRight : 'none' , borderLeft : 'none' }} transition={{ ease: 'linear' , type: 'spring' , stiffness: 1000 , duration: .5 }}>Skills</motion.div>
+        </div>
+      </nav>
+
+      <section className='herosection'>
+          <div className='profile'>
+            <motion.div initial={{ scale:0 }} whileInView={{ scale: 1 }} whileTap={{ cursor: 'grabbing' ,scale: 0.92 }} transition={{ ease: 'linear' , type: 'spring' , stiffness: 100 }} drag dragConstraints = {{ left: 0 , right: 0 , bottom: 0 , top: 0 }} className='image'></motion.div>
+            <motion.div initial={{ scale: 0 }} whileInView={{ scale: 1 }} className='name'> Marvellous Fadunmiye </motion.div>
+          </div>
+          <div className='description'>
+            Web Developer & Instructor
+          </div>
+      </section>
+
     </div>
   );
 }
